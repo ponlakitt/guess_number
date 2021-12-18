0d53cba084498c4f7b30fef88d6d76e71f7b14cd
Ponlakit Konkrathok <Ponlakit13@hotmail.com>
2021-12-18 22:40:41 +0700

    partial fix

--- lib/test.dart
+++ lib/test.dart
@@ -1,8 +1,43 @@
 // top-level function
+// ignore_for_file: avoid_print
 import 'dart:io';
+import 'dart:math';
 
-void main(){
-  var input  = stdin.readLineSync();
+void main() {
+  var i = 0;
+  var r = Random();
+  var answer = r.nextInt(100);
+  bool correct = true;
+  print("╔══════════════════════════════════════════════════");
+  print("║                 GUESS THE NUMBER ☺ ");
+  print("║──────────────────────────────────────────────────");
+
+  while (correct == true) {
+    stdout.write('║ Guess the number between 1 and 100:');
+    var input = stdin.readLineSync();
+    var guess = int.tryParse(input!);
+
+    if (guess != null) {
+      if (guess == answer) {
+        correct = false;
+      }
+      else if (guess < answer) {
+        print("║ ➜ " + guess.toString() +
+            " is too low! ▼ try again.");
+        print("║──────────────────────────────────────────────────");
+      }
+      else if (guess > answer) {
+        print("║ ➜ " + guess.toString() +
+            " is too high! ▲ try again.");
+        print("║──────────────────────────────────────────────────");
+      }
+      i++;
+    }
+  }
+
+  print("║ ➜ " + answer.toString() + " is Congrat! ❤,You guess " + i.toString() + " times.");
+  print("║──────────────────────────────────────────────────");
+  print("║                     THE END");
+  print("╚═══════════════════════════════════════════════════");
 }
-
-// รับ input
+// รับ input ทาง cmd แล้วแสดงผล
